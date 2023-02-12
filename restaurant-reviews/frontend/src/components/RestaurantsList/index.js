48721740fcede4cb2a2914e13de61248a733259f
Rafael Ederli <rafaederli@gmail.com>
2023-02-12 01:21:52 -0300

    updating modules

--- restaurant-reviews/frontend/src/components/RestaurantsList/index.js
+++ restaurant-reviews/frontend/src/components/RestaurantsList/index.js
@@ -1,9 +1,86 @@
-function RestaurantsList() {
-    return (
-        <div>
-            Restaurants List
-        </div>
-    );
-}
+import React, { useState, useEffect } from "react";
+import RestaurantDataService from "../services/restaurant";
+import { Link } from "react-router-dom";
+
+const RestaurantsList = props => {
+    const [restaurants, setRestaurants] = useState([]);
+    const [searchName, setSearchName] = useState("");
+    const [searchZip, setSearchZip] = useState("");
+    const [searchCuisine, setSearchCuisine] = useState("");
+    const [cuisines, setCuisines] = useState(["All Cuisines"]);
+
+    useEffect(() => {
+        retrieveRestaurants();
+        retrieveCuisines();
+    }, []);
+
+    const onChangeSearchName = e => {
+        const searchName = e.target.value;
+        setSearchName(searchName);
+    };
+
+    const onChangeSearchZip = e => {
+        const searchZip = e.target.value;
+        setSearchZip(searchZip);
+    };
+
+    const onChangeSearchCuisine = e => {
+        const searchCuisine = e.target.value;
+        setSearchCuisine(searchCuisine);
+    }
+
+    const retrieveRestaurants = () => {
+        RestaurantDataService.getAll()
+            .then(response => {
+                console.log(response.data);
+                setRestaurants(response.data.restaurants);
+            })
+            .catch(e => {
+                console.log(e);
+            });
+    };
+
+    const retrieveCuisines = () => {
+        RestaurantDataService.getCuisines()
+            .then(response => {
+                console.log(response.data);
+                setCuisines(["All Cuisines".concat(response.data)]);
+            })
+            .catch(e => {
+                console.log(e);
+            })
+    };
+
+    const refreshList = () => {
+        retrieveRestaurants();
+    };
+
+    const find = (query, by) => {
+        RestaurantDataService.find(query, by)
+            .then(response => {
+                console.log(response.data);
+                setRestaurants(response.data.restaurants);
+            })
+            .catch(e => {
+                console.log(e);
+            });
+    };
+
+    const findByName = () => {
+        find(searchName, "name");
+    };
+
+    const findByZip = () => {
+        find(searchZip, "zipcode");
+    };
+
+    const findByCuisine = () => {
+        if (searchCuisine == "All Cuisines") {
+            refreshList();
+        } else {
+            find(searchCuisine, "cuisine");
+        };
+    };
+};
 
 export default RestaurantsList;
