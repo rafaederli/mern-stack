bf3e66d6111cbc8936fb159f9726c9465d6c42d0
Rafael Ederli <rafaederli@gmail.com>
2023-02-13 16:46:54 -0300

    frontend update

--- restaurant-reviews/frontend/src/components/AddReview/index.js
+++ restaurant-reviews/frontend/src/components/AddReview/index.js
@@ -1,12 +1,95 @@
 import { useState } from "react";
 import RestaurantDataService from "../../services/restaurant.js";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
-const AddReview = () => {
+const AddReview = props => {
     
+    const { id } = useParams()
+
+    let initialReviewState = ""
+
+    let editing = false;
+
+    if (props.state && props.state.currentReview) {
+        editing = true;
+        initialReviewState = props.state.currentReview.text;
+    }
+
+    const [review, setReview] = useState(initialReviewState);
+    const [submitted, setSubmitted] = useState(false);
+
+    const handleInputChange = event => {
+        setReview(event.target.value);
+    };
+
+    const saveReview = () => {
+        var data = {
+            text: review,
+            name: props.user.name,
+            user_id: props.user.id,
+            restaurant_id: id
+        };
+
+        if (editing) {
+            data.review_id = props.state.currentReview._id
+            RestaurantDataService.updateReview(data)
+                .then(response => {
+                    setSubmitted(true);
+                    console.log(response.data);
+                })
+                .catch(e => {
+                    console.log(e);
+                });
+        } else {
+            RestaurantDataService.createReview(data)
+                .then(response => {
+                    setSubmitted(true);
+                    console.log(response.data);
+                })
+                .catch(e => {
+                    console.log(e);
+                });
+        }
+
+    }
+
     return (
         <div>
-            Add Review Page
+            {
+                props.user ? (
+                    <div className="submit-form">
+                        {submitted ? (
+                            <div>
+                                <h4>You submitted successfully!</h4>
+                                <Link to={"/restaurants/" + id} className="btn btn-success">
+                                    Back to Restaurant
+                                </Link>
+                            </div>
+                        ) : (
+                            <div>
+                                <div className="form-group">
+                                    <label htmlForm="description">{ editing ? "Edit" : "Create" } Review</label>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        id="text"
+                                        required
+                                        value={review}
+                                        onChange={handleInputChange}
+                                        name="text"
+                                    />
+                                </div>
+                                <button onClick={saveReview} className="btn btn-success">
+                                    Submit
+                                </button>
+                            </div>
+                        )}
+                    </div>
+                ) : (
+                    <div>
+                        Please log in.
+                    </div>
+                )}
         </div>
     );
 }
