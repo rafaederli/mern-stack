9f47a0b38ea09f0b42113f92ff402bfdab3c0017
Rafael Ederli <rafaederli@gmail.com>
2023-02-17 11:49:08 -0300

    frontend update: components modification, features creation and routes update

--- tech-notes/tech-notes-frontend/src/App.js
+++ tech-notes/tech-notes-frontend/src/App.js
@@ -1,7 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Public from "./components/Public";
-import Login from "./component/Login";
+import Login from "./features/auth/Login";
+import DashLayout from "./components/DashLayout";
+import Welcome from "./features/auth/Welcome";
+import NotesList from "./features/notes/NotesList";
+import UsersList from "./features/users/UsersList";
 
 function App() {
   return (
@@ -9,6 +13,15 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Public />} />
         <Route path="login" element={<Login />} />
+        <Route path="dash" element={<DashLayout />}>
+          <Route index element={<Welcome />} />
+          <Route path="notes">
+            <Route index element={<NotesList />} />
+          </Route>
+          <Route path="users">
+            <Route index element={<UsersList />} />
+          </Route>
+        </Route>{/* End Dash */}
       </Route>
     </Routes>
   );
